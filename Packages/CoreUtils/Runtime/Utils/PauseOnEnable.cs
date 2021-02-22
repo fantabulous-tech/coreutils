f864c83fb6aec7eeeaea9975ecc4d3fece2b7629
Fantabulous <fan@fantabulous.tech>
2021-02-23 10:23:36 +1300

    [PauseOnEnable] Support out-of-order enable/disables

--- Packages/CoreUtils/Runtime/Utils/PauseOnEnable.cs
+++ Packages/CoreUtils/Runtime/Utils/PauseOnEnable.cs
@@ -1,19 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace CoreUtils {
     public class PauseOnEnable : MonoBehaviour {
+        private static readonly List<PauseOnEnable> s_ActivePauses = new List<PauseOnEnable>();
+
         [SerializeField] private float m_PauseSpeed = 0.0001f;
 
         private void OnEnable() {
-            OnPauseChanged(true);
+            if (!s_ActivePauses.Contains(this)) {
+                s_ActivePauses.Add(this);
+                UpdatePause();
+            } else {
+                Debug.LogError("PauseOnEnable tried to add a pause that already exists.", this);
+            }
         }
 
         private void OnDisable() {
-            OnPauseChanged(false);
+            if (s_ActivePauses.Contains(this)) {
+                s_ActivePauses.Remove(this);
+                UpdatePause();
+            } else {
+                Debug.LogError("PauseOnEnable tried to remove a pause that didn't exist.", this);
+            }
         }
 
-        private void OnPauseChanged(bool isPaused) {
-            Time.timeScale = isPaused ? m_PauseSpeed : 1;
+        private void UpdatePause() {
+            PauseOnEnable lastPause = s_ActivePauses.LastOrDefault();
+            Time.timeScale = lastPause ? lastPause.m_PauseSpeed : 1;
         }
     }
 }
