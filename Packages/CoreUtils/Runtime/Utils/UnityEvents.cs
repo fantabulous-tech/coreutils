442882f8a1ee3eef88c6a416b711d06f7c5420af
Fantabulous <fan@fantabulous.tech>
2021-11-27 14:57:01 +1300

    Add 'OnEnableChangedEvent' to UnityEvents

--- Packages/CoreUtils/Runtime/Utils/UnityEvents.cs
+++ Packages/CoreUtils/Runtime/Utils/UnityEvents.cs
@@ -5,13 +5,16 @@ namespace CoreUtils {
     public class UnityEvents : MonoBehaviour {
         public UnityEvent OnEnableEvent;
         public UnityEvent OnDisableEvent;
+        public UnityEventBool OnEnableChangedEvent;
 
         private void OnEnable() {
             OnEnableEvent.Invoke();
+            OnEnableChangedEvent.Invoke(true);
         }
 
         private void OnDisable() {
             OnDisableEvent.Invoke();
+            OnEnableChangedEvent.Invoke(false);
         }
     }
 }
