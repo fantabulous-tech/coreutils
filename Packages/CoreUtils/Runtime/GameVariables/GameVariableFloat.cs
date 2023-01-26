e8928f7126a1c1c1ea7976526e98750815d79058
Fantabulous <fan@fantabulous.tech>
2023-01-26 10:39:46 -0800

    Add invariant culture to float and int parsing

--- Packages/CoreUtils/Runtime/GameVariables/GameVariableFloat.cs
+++ Packages/CoreUtils/Runtime/GameVariables/GameVariableFloat.cs
@@ -1,10 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Globalization;
+using UnityEngine;
 
 namespace CoreUtils.GameVariables {
-    [CreateAssetMenu(menuName = "CoreUtils/GameVariable/Float", order = (int) MenuOrder.VariableFloat)]
+    [CreateAssetMenu(menuName = "CoreUtils/GameVariable/Float", order = (int)MenuOrder.VariableFloat)]
     public class GameVariableFloat : BaseGameVariable<GameVariableFloat, float> {
+
+        public override string ValueString {
+            get => GetValue().ToString(CultureInfo.InvariantCulture);
+            set => Value = Parse(value);
+        }
+
         protected override float Parse(string stringValue) {
-            return float.Parse(stringValue);
+            return float.Parse(stringValue, CultureInfo.InvariantCulture);
         }
 
         protected override bool Equals(float a, float b) {
--- Packages/CoreUtils/Runtime/GameVariables/GameVariableInt.cs
+++ Packages/CoreUtils/Runtime/GameVariables/GameVariableInt.cs
@@ -1,10 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Globalization;
+using UnityEngine;
 
 namespace CoreUtils.GameVariables {
-    [CreateAssetMenu(menuName = "CoreUtils/GameVariable/Int", order = (int) MenuOrder.VariableInt)]
+    [CreateAssetMenu(menuName = "CoreUtils/GameVariable/Int", order = (int)MenuOrder.VariableInt)]
     public class GameVariableInt : BaseGameVariable<GameVariableInt, int> {
+
+        public override string ValueString {
+            get => GetValue().ToString(CultureInfo.InvariantCulture);
+            set => Value = Parse(value);
+        }
+
         protected override int Parse(string stringValue) {
-            return int.TryParse(stringValue, out int result) ? result : 0;
+            return int.TryParse(stringValue, NumberStyles.Integer, CultureInfo.InvariantCulture, out int result) ? result : 0;
         }
     }
 }
