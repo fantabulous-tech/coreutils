dc3e3827ded37de0351d05ffbc9acdb316a25698
Fantabulous <fan@fantabulous.tech>
2023-01-26 10:38:49 -0800

    Handle cases when child transforms can't be reset

--- Packages/CoreUtils/Editor/Utils/PrefabUtils.cs
+++ Packages/CoreUtils/Editor/Utils/PrefabUtils.cs
@@ -1,21 +1,30 @@
+using System;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 namespace CoreUtils.Editor {
     public static class PrefabUtils {
-        [MenuItem("Tools/CoreUtils/Reset Child Transform Overrides", true, (int) MenuOrder.GameObject)]
+        [MenuItem("Tools/CoreUtils/Reset Child Transform Overrides", true, (int)MenuOrder.GameObject)]
         public static bool CanResetTransformOverrides() {
             return Selection.gameObjects.Any(EditorUtils.IsInstance);
         }
 
-        [MenuItem("Tools/CoreUtils/Reset Child Transform Overrides", false, (int) MenuOrder.GameObject)]
+        [MenuItem("Tools/CoreUtils/Reset Child Transform Overrides", false, (int)MenuOrder.GameObject)]
         private static void ResetTransformOverrides() {
-            foreach (Transform t in Selection.activeTransform.GetComponentsInChildren<Transform>()) {
-                if (!t) {
-                    continue;
+            foreach (Transform activeTransform in Selection.transforms) {
+                foreach (Transform t in activeTransform.GetComponentsInChildren<Transform>()) {
+                    if (!t) {
+                        continue;
+                    }
+
+                    try {
+                        PrefabUtility.RevertObjectOverride(t, InteractionMode.UserAction);
+                    }
+                    catch (Exception e) {
+                        Debug.Log($"Couldn't revert {t.name} because {e.Message}", t);
+                    }
                 }
-                PrefabUtility.RevertObjectOverride(t, InteractionMode.UserAction);
             }
         }
     }
